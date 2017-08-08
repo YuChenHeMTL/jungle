2a51c1833705e17a8eb1fd66272841abcd53833f
YuChenHeMTL <yuchen7568@gmail.com>
2017-08-08 20:25:15 +0000

    removed username migration

--- db/migrate/20170802234453_add_first_and_last_name.rb
+++ db/migrate/20170802234453_add_first_and_last_name.rb
@@ -1,8 +1,5 @@
 class AddFirstAndLastName < ActiveRecord::Migration
   def change
-    remove_column :users, :username
-    remove_column :users, :password
-    remove_column :users, :password_confirmation
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
   end
--- db/schema.rb
+++ db/schema.rb
@@ -70,9 +70,12 @@ ActiveRecord::Schema.define(version: 20170803213924) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
     t.string   "password_digest"
+    t.string   "password_confirmation"
+    t.string   "password"
+    t.string   "username"
     t.string   "first_name"
     t.string   "last_name"
   end
